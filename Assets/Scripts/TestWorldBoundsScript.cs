f581445bd5342d1f2983ba0fc5d08150002e86ac
Charles Carter <cmcrter@gmail.com>
2020-08-01 15:48:38 +0000

    Visuals, teleport if out of world, test slopes

---
+++ Assets/Scripts/TestWorldBoundsScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestWorldBoundsScript : MonoBehaviour
+{
+    //When something enters the bounds
+    private void OnTriggerEnter(Collider other)
+    {
+        //Put it back at the start
+        other.transform.position = new Vector3(0, 5, 0);
+    }
+}
