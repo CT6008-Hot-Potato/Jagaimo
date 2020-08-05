10403541a786099ef07bbf329e6e16a264680b43
Charles Carter <cmcrter@gmail.com>
2020-08-05 15:30:38 +0000

    should we have our own fps counter made?

---
+++ Assets/Scripts/Monobehaviour/FPScounter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//We may want to make our own FPS Display at some point
+public class FPScounter : MonoBehaviour
+{
+    int current_fps;
+    int highest_fps;
+    int lowest_fps;
+
+    Text fpsText;
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
