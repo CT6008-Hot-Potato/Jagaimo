099ba50ca38cd2198bc3abf9780b39657134f003
Charles Carter <cmcrter@gmail.com>
2020-08-15 17:33:58 +0000

    Edit mode testing + correct assembly definition

---
+++ Assets/_project/Scripts/TestS/EditModeTests/TimerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TimerTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TimerTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TimerTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+
+        [Test]
+        public void TimerWorks()
+        {
+            //Assign
+            var timer = new Timer(1.0f);
+            
+            //Act
+            timer.Tick(1.0f);
+
+            //Assert
+            Assert.AreEqual(timer.current_time, 0.0f);
+            Assert.AreEqual(timer.isActive, false);
+        }
+    }
+}
