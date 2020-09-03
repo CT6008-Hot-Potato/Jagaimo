1e0222600696c929f105c35e635d2afebe3c73e8
Unkoun <unkoun98@gmail.com>
2020-09-03 12:25:22 +0000

    No Comments

---
+++ Assets/_project/Scripts/Lobby Scripts/NetworkManagerLobby.cs
@@ -0,0 +1,166 @@
+﻿using Mirror;
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetworkManagerLobby : NetworkManager
+{
+    // Start is called before the first frame update
+    [Scene] [SerialzeField] private string menuScene = string.Empty;
+    [Header("Room")]
+    [SerializeField] private NetworkRoomPlayerLobby roomPlayerPrefab = null;
+
+    public static event Action OnClientConnected;
+    public static event Action OnClientDisconnected;
+
+
+    public override void OnStartSever() => spawnPrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs".ToList());
+
+    public override void OnStartClient()
+    {
+        var spawnablePrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs");
+        foreach (var prefab in spawnablePrefabs)
+        {
+            ClientScene.RegisterPrefab(prefab);
+        }
+    }
+
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        OnClientConnected?.Invoke();
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        base.OnClientDisconnect(conn);
+
+        OnClientDisconnected?.Invoke(); 
+    }
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        if (numPlayers >= maxConnections)
+        {// disconnects player when there are too many players
+            conn.Disconnect();
+            return;
+        }
+
+        if (SceneManager.GetActiveScene().name != menuScene)
+        {// stops people joining while game is in progress
+            conn.Disconnect();
+            return;
+        }
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        if (SceneManager.GetActiveScene().name == menuScene)
+        {
+            bool isLeader = RoomPlayers.Count == 0;
+
+            NetworkRoomPlayerLobby roomPlayerInstance = Instantiate(roomPlayerPrefab);
+
+            roomPlayerInstance.IsLeader = isLeader;
+
+            NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
+        }
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        if (conn.identity != null)
+        {
+            var player = conn.identity.GetComponent<NetworkRoomPlayerLobby>();
+
+            RoomPlayers.Remove(player);
+
+            NotifyPlayersOfReadyState();
+        }
+
+        base.OnServerDisconnect(conn);
+    }
+
+    public override void OnStopServer()
+    {
+        OnServerStopped?.Invoke();
+
+        RoomPlayers.Clear();
+        GamePlayers.Clear();
+    }
+
+    public void NotifyPlayersOfReadyState()
+    {
+        foreach (var player in RoomPlayers)
+        {
+            player.HandleReadyToStart(IsReadyToStart());
+        }
+    }
+
+    private bool IsReadyToStart()
+    {
+        if (numPlayers < minPlayers) { return false; }
+
+        foreach (var player in RoomPlayers)
+        {
+            if (!player.IsReady) { return false; }
+        }
+
+        return true;
+    }
+
+    public void StartGame()
+    {
+        if (SceneManager.GetActiveScene().name == menuScene)
+        {
+            if (!IsReadyToStart()) { return; }
+
+            mapHandler = new MapHandler(mapSet, numberOfRounds);
+
+            ServerChangeScene(mapHandler.NextMap);
+        }
+    }
+
+    public override void ServerChangeScene(string newSceneName)
+    {
+        // From menu to game
+        if (SceneManager.GetActiveScene().name == menuScene && newSceneName.StartsWith("Scene_Map"))
+        {
+            for (int i = RoomPlayers.Count - 1; i >= 0; i--)
+            {
+                var conn = RoomPlayers[i].connectionToClient;
+                var gameplayerInstance = Instantiate(gamePlayerPrefab);
+                gameplayerInstance.SetDisplayName(RoomPlayers[i].DisplayName);
+
+                NetworkServer.Destroy(conn.identity.gameObject);
+
+                NetworkServer.ReplacePlayerForConnection(conn, gameplayerInstance.gameObject);
+            }
+        }
+
+        base.ServerChangeScene(newSceneName);
+    }
+
+    public override void OnServerSceneChanged(string sceneName)
+    {
+        if (sceneName.StartsWith("Scene_Map"))
+        {
+            GameObject playerSpawnSystemInstance = Instantiate(playerSpawnSystem);
+            NetworkServer.Spawn(playerSpawnSystemInstance);
+
+            GameObject roundSystemInstance = Instantiate(roundSystem);
+            NetworkServer.Spawn(roundSystemInstance);
+        }
+    }
+
+    public override void OnServerReady(NetworkConnection conn)
+    {
+        base.OnServerReady(conn);
+
+        OnServerReadied?.Invoke(conn);
+    }
+}
+}
---
+++ Assets/_project/Scripts/Lobby Scripts/NetworkRoomPlayerLobby.cs
@@ -0,0 +1,15 @@
+﻿
+/////////////////////////////////////////////////////////////
+//
+//  Script Name: MenuManager.cs
+//  Developed by: James Bradbury
+//  Description: From https://www.youtube.com/watch?v=Fx8efi2MNz0&list=PLS6sInD7ThM1aUDj8lZrF4b4lpvejB2uB&index=5
+//  
+/////////////////////////////////////////////////////////////
+///
+
+using Mirror;
+public class NetworkRoomPlayerLobby : NetworkBehaviour
+{
+   
+}
