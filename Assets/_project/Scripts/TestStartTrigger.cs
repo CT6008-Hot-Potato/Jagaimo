c6ee0c7a91d12f7bd07e24fedf23730e3edbe5cb
CABgames <charlieAbullock@gmail.com>
2021-02-12 14:38:51 +0000

    .gitignore and new branch

--- Assets/_project/Scripts/TestStartTrigger.cs
+++ Assets/_project/Scripts/TestStartTrigger.cs
@@ -1,21 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 //A class so I can start the game whenever I want to in the test scene
 public class TestStartTrigger : MonoBehaviour
 {
+    //Variables
     //The object that kicks the round off
     [SerializeField]
     BasicTimerBehaviour startCountdown;
-
+    [SerializeField]
+    private PlayerInput playerInput = null;
+    public PlayerInput PlayerInput => playerInput;
+    private float playValue = 0;
     // Update is called once per frame
     void Update()
     {
         //P for play game
-        if (Input.GetKeyDown(KeyCode.P))
+        if (playValue > 0.1f)
         {
             startCountdown.CallOnTimerStart();
         }
     }
+
+    public void Play(InputAction.CallbackContext ctx)
+    {
+        playValue = ctx.ReadValue<float>();
+    }
+
 }
