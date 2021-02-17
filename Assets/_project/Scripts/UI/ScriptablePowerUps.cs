02d60dce7a1c96a4cfa7a5b1aa40b5881f36dec2
James Bradbury <unkoun98@gmail.com>
2021-02-17 20:38:41 +0000

    week 5 progress
    Ticker text & powerup ui progress

---
+++ Assets/_project/Scripts/UI/ScriptablePowerUps.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[CreateAssetMenu(fileName = "PowerUps", menuName = "ScriptableObjects/PowerUps", order = 2)]
+public class ScriptablePowerUps : ScriptableObject
+{
+    [Serializable]
+    public class PowerUp
+    {
+        public string name;
+        public Sprite SpriteImage; 
+    }
+    public PowerUp[] powerUps;
+}
