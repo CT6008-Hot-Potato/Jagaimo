1bc5e4ceb87733da783811f7dcd33ff788150cc2
SamBeevor <78489412+SamBeevor@users.noreply.github.com>
2021-04-16 16:27:41 +0100

    Merge branch 'master' into Sam_2

---
+++ Assets/_project/Scripts/Managers/ScriptableSounds.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[CreateAssetMenu(fileName = "SoundProfile", menuName = "ScriptableObjects/SoundProfile", order = 1)]
+public class ScriptableSounds : ScriptableObject
+{
+    // Scriptable Object containing all the potential palette options
+    public enum Sounds // All registered game sounds 
+    {
+        None,
+        ButtonPress,
+        Explosion,
+        PowerUp,
+        MenuMusic,
+        Grabbing,
+        Throwing,
+        Sliding,
+        Crouching,
+        Jumping
+    };
+
+
+
+    [Serializable]
+    public class SoundIdentity 
+    {
+        public Sounds name;
+        public AudioClip[]  file;
+        public AudioMixerGroup audioMixer;
+        public float defaultVolume;
+        public float pitchVariation;
+    }
+
+    public SoundIdentity[] soundPalette;    // array of all palettes
+
+   public SoundIdentity GetSoundFromPalette( Sounds soundName)
+    {
+        SoundIdentity returnSound = new SoundIdentity();
+
+        foreach (SoundIdentity i in soundPalette)
+        {
+            if(i.name == soundName)
+            {
+                returnSound = i;
+                return i;
+            }
+        }
+        return returnSound;
+    }
+
+    
+
+}
+
