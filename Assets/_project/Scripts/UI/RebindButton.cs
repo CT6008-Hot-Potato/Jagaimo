934efac94a50e09f268b3fc0eb7533da13a8ee32
James Bradbury <unkoun98@gmail.com>
2021-04-22 15:57:09 +0100

    buttons for rebinding

---
+++ Assets/_project/Scripts/UI/RebindButton.cs
@@ -0,0 +1,34 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///RebindButton.cs
+///Developed by James Bradbury
+///
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RebindButton : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI actionName, buttonName;
+
+    public InputAction currentBindingInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    public void UpdateDisplay(string action, string button, InputAction BindingInput)
+    {
+        actionName.text = action;
+        buttonName.text = button;
+
+        currentBindingInput = BindingInput;
+    }
+}
---
+++ Assets/_project/Scripts/UI/RebindPanelManager.cs
@@ -0,0 +1,46 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///RebindPanelManager.cs
+///Developed by James Bradbury
+///
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RebindPanelManager : MonoBehaviour
+{
+    [SerializeField] GameObject rebindObject;
+    public InputActionAsset ControlScheme;  // put new Input manager here
+
+    public string MapName; // Name of the list of inputs being changed. (player 1, player 2, etc)
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (InputAction i in ControlScheme.FindActionMap(MapName).actions)
+        {
+            if (i.type == InputActionType.Button)
+            {
+                GameObject j = Instantiate(rebindObject, transform);
+
+                if (j.TryGetComponent(out RebindButton button))
+                {
+
+                    button.UpdateDisplay(i.name, i.GetBindingDisplayString(InputBinding.DisplayStringOptions.DontIncludeInteractions), i);
+
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
