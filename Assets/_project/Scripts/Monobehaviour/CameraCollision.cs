1d25c8519a2ea8961cdbc7e74dc640223af62318
CABgames <S1707038@glos.ac.uk>
2021-04-23 06:01:31 +0100

    Update
    Rebinding of some player controller stuff
    Tweaking freecam to work better for controller
    Smoother transition for wall jump and making it feel much better
    Shoulder buttons for zoom in/out now
    Spectator mode collision now returns players to centre
    Fog effect on map
    Other tweaks

---
+++ Assets/_project/Scripts/Monobehaviour/CameraCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    private void OnCollisionStay(Collision collision)
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        transform.position = Vector3.zero;
+    }
+}
