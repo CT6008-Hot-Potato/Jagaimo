fbf00c6ea51661973533aa9f5962efb1cc957ae3
Theodor D <40744419+Motionsense@users.noreply.github.com>
2021-04-24 18:04:11 +0100

    Multiplayer Lobby logic, such as hosting and joining lobbies, perhas needs some further testing

---
+++ Assets/Mirror/Jagaimo Network Scripts/JagaimoNetworkManagerLobby.cs
@@ -0,0 +1,189 @@
+﻿/////////////////////////////////////////////////////////////
+//  File: JagaimoNetworkManagerLobby.cs
+//  Creator: Theodor Danciu
+//  Brief: Logic that handles the network connections and the objects that need to sent over the network once the lobby is initialised
+/////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mirror;
+using UnityEngine;
+
+public class JagaimoNetworkManagerLobby : Mirror.NetworkManager
+{
+    [SerializeField] private int minPlayers = 2;
+    
+    [Scene] 
+    [SerializeField] private string menuScene = String.Empty;
+
+    [Header("Room")] 
+    [SerializeField] private JagaimoRoomPlayerLobby roomPlayerPrefab = null;
+
+    public static event Action OnClientConnected;
+    public static event Action OnClientDisconnected;
+    public static event Action<NetworkConnection> OnServerReadied;
+    public static event Action OnServerStopped;
+    
+    public List<JagaimoRoomPlayerLobby> RoomPlayers { get; } = new List<JagaimoRoomPlayerLobby>();
+   public List<JagaimoRoomPlayerLobby> GamePlayers { get; } = new List<JagaimoRoomPlayerLobby>();
+
+   /// <summary>
+   /// When server is hosted, make sure the list of the prefabs that are to be spawned is emptied first,
+   /// then add the lobby prefabs into the list
+   /// </summary>
+   public override void OnStartServer()
+   {
+       spawnPrefabs.Clear();
+       spawnPrefabs = Resources.LoadAll<GameObject>("Spawnables").ToList();
+   }
+
+   /// <summary>
+   /// When a client joins the lobby, the list of the prefabs that are to be registered and spawned is emptied first,
+   /// load the lobby prefabs into the list and register each prefab so it can be used over the network
+   /// </summary>
+public override void OnStartClient()
+   {
+       spawnPrefabs.Clear();
+       spawnPrefabs = Resources.LoadAll<GameObject>("Spawnables").ToList();
+
+       NetworkClient.ClearSpawners();
+
+       foreach (GameObject prefab in spawnPrefabs)
+       {
+           NetworkClient.RegisterPrefab(prefab);
+       }
+   }
+
+    /// <summary>
+    /// Server connection handler when a client is attempting to join a lobby
+    /// </summary>
+    /// <param name="conn">Client's connection attempt</param>
+    public override void OnClientConnect(Mirror.NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+        OnClientConnected?.Invoke();
+    }
+
+    /// <summary>
+    /// Server connection handler when a current player is leaving the lobby
+    /// </summary>
+    /// <param name="conn">Client's connection attempt</param>
+    public override void OnClientDisconnect(Mirror.NetworkConnection conn)
+    {
+        base.OnClientDisconnect(conn);
+        OnClientDisconnected?.Invoke();
+    }
+
+    /// <summary>
+    /// Server connection handler if the room that the client is trying to connect is full or when the client's current scene is different thant the server's
+    /// </summary>
+    /// <param name="conn">Client's connection attempt</param>
+    public override void OnServerConnect(Mirror.NetworkConnection conn)
+    {
+        if (numPlayers >= maxConnections)
+        {
+            conn.Disconnect();
+            return;
+        }
+
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().path != menuScene)
+        {
+            conn.Disconnect();
+            return;
+        }
+    }
+
+    /// <summary>
+    /// Server connection handler to instantiate a new player prefab when the client's connection attempt is a success
+    /// </summary>
+    /// <param name="conn">Client's connection attempt</param>
+    public override void OnServerAddPlayer(Mirror.NetworkConnection conn)
+    {
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().path == menuScene)
+        {
+            bool isLeader = RoomPlayers.Count == 0;
+
+            JagaimoRoomPlayerLobby roomPlayerInstance = Instantiate(roomPlayerPrefab);
+
+            roomPlayerInstance.IsLeader = isLeader;
+            
+            Mirror.NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
+        }
+    }
+    
+    /// <summary>
+    /// When the player host closes the lobby, make sure other players that have joined are removed from the server
+    /// </summary>
+    /// <param name="conn">Client's connection attempt</param>
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        if (conn.identity != null)
+        {
+            JagaimoRoomPlayerLobby player = conn.identity.GetComponent<JagaimoRoomPlayerLobby>();
+
+            RoomPlayers.Remove(player);
+
+            NotifyPlayersOfReadyState();
+        }
+
+        base.OnServerDisconnect(conn);
+    }
+    
+    /// <summary>
+    /// When the server is stopped, clear the list that had the players' details
+    /// </summary>
+    public override void OnStopServer()
+    {
+        OnServerStopped?.Invoke();
+
+        RoomPlayers.Clear();
+        //GamePlayers.Clear();
+    }
+
+    /// <summary>
+    /// Notifies the other players from the lobby if a player has readied up
+    /// </summary>
+    public  void NotifyPlayersOfReadyState()
+    {
+        foreach (JagaimoRoomPlayerLobby player in RoomPlayers)
+        {
+            player.HandleReadyToStart(IsReadyToStart());
+        }
+    }
+    
+    /// <summary>
+    /// Checks if any player has readied player
+    /// </summary>
+    /// <returns> Player is ready </returns>
+    private bool IsReadyToStart()
+    {
+        if (numPlayers < minPlayers) { return false; }
+
+        foreach (var player in RoomPlayers)
+        {
+            if (!player.IsReady) { return false; }
+        }
+
+        return true;
+    }
+
+    public override void OnServerReady(NetworkConnection conn)
+    {
+        base.OnServerReady(conn);
+
+        OnServerReadied?.Invoke(conn);
+    }
+    
+    // public void StartGame()
+    // {
+    //     if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == menuScene)
+    //     {
+    //         if (!IsReadyToStart()) { return; }
+    //
+    //         mapHandler = new MapHandler(mapSet, numberOfRounds);
+    //
+    //         ServerChangeScene(mapHandler.NextMap);
+    //     }
+    // }
+}
---
+++ Assets/Mirror/Jagaimo Network Scripts/JagaimoRoomPlayerLobby.cs
@@ -0,0 +1,173 @@
+﻿/////////////////////////////////////////////////////////////
+//  File: JagaimoNRoomPlayerLobby.cs
+//  Creator: Theodor Danciu
+//  Brief: Logic that handles the updating state of the lobby including names and weather the players are ready uped 
+/////////////////////////////////////////////////////////////
+
+using Mirror;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JagaimoRoomPlayerLobby : NetworkBehaviour
+{
+    [Header("UI")] 
+    [SerializeField] private GameObject lobbyUI = null;
+    [SerializeField] private TMP_Text[] playerNameTexts = new TMP_Text[4];
+    [SerializeField] private TMP_Text[] playerReadyTexts = new TMP_Text[4];
+    [SerializeField] private Button startGameButton = null;
+
+    [SyncVar(hook = nameof(HandleDisplayNameChanged))]
+    public string DisplayName = "Loading...";
+
+    [SyncVar(hook = nameof(HandleReadyStatusChanged))]
+    public bool IsReady = false;
+
+    private bool isLeader;
+
+    /// <summary>
+    /// This is to be used when the lobby is hosted and the player that hosts it becomes the leader
+    /// </summary>
+    public bool IsLeader
+    {
+        set
+        {
+            isLeader = value;
+            startGameButton.gameObject.SetActive(value);
+        }
+    }
+
+    private JagaimoNetworkManagerLobby room;
+
+    private JagaimoNetworkManagerLobby Room
+    {
+        get
+        {
+            if (room != null)
+            {
+                return room;
+            }
+
+            return room = NetworkManager.singleton as JagaimoNetworkManagerLobby;
+        }
+    }
+    
+    public override void OnStartAuthority()
+    {
+        CmdSetDisplayName(JagaimoPlayerNameInput.DisplayName);
+
+        lobbyUI.SetActive(true);
+    }
+
+    /// <summary>
+    /// When a player attempts to join the lobby and is successful, add the player in the lobby's list and update it's display - name and ready status
+    /// </summary>
+    public override void OnStartClient()
+    {
+        Room.RoomPlayers.Add(this);
+
+        UpdateDisplay();
+    }
+
+    /// <summary>
+    /// When a player leaves the lobby, the player is removed from the lobby's list and the slot's that was used by the player updates its text
+    /// </summary>
+    public override void OnStopClient()
+    {
+        Room.RoomPlayers.Remove(this);
+
+        UpdateDisplay();
+    }
+
+    /// <summary>
+    /// Logic that handles the ready status when a player from the lobby has readied up
+    /// </summary>
+    /// <param name="oldValue"> Old state of the player's ready status in the previous frame </param>
+    /// <param name="newValue"> Current state of the player's ready status updated </param>
+    public void HandleReadyStatusChanged(bool oldValue, bool newValue) => UpdateDisplay();
+    
+    /// <summary>
+    /// Logic that handles the display name that updates if a new player joins the lobby
+    /// </summary>
+    /// <param name="oldValue"> Old state of the player's name text field in the previous frame </param>
+    /// <param name="newValue"> Current state of the player's name text field updated </param>
+    public void HandleDisplayNameChanged(string oldValue, string newValue) => UpdateDisplay();
+
+    /// <summary>
+    /// Checks whether the player has authority over the object initially,
+    /// if not check go through each player in the lobby and update its display - name and ready status.
+    /// </summary>
+    private void UpdateDisplay()
+    {
+        if (!hasAuthority)
+        {
+            foreach (var player in Room.RoomPlayers)
+            {
+                if (player.hasAuthority)
+                {
+                    player.UpdateDisplay();
+                    break;
+                }
+            }
+
+            return;
+        }
+
+        for (int i = 0; i < playerNameTexts.Length; i++)
+        {
+            playerNameTexts[i].text = "Waiting For Player...";
+            playerReadyTexts[i].text = string.Empty;
+        }
+
+        for (int i = 0; i < Room.RoomPlayers.Count; i++)
+        {
+            playerNameTexts[i].text = Room.RoomPlayers[i].DisplayName;
+            playerReadyTexts[i].text = Room.RoomPlayers[i].IsReady ? "<color=green>Ready</color>" : "<color=red>Not Ready</color>";
+        }
+    }
+
+    /// <summary>
+    /// Checks if everyone from the lobby has readied up by making the start game intractable 
+    /// </summary>
+    /// <param name="readyToStart"> Lobby's general ready status </param>
+    public void HandleReadyToStart(bool readyToStart)
+    {
+        if (!isLeader)
+        {
+            return;
+        }
+
+        startGameButton.interactable = readyToStart;
+    }
+
+    /// <summary>
+    /// Sends over the network a command to the server to display the player's name when a new player joins the lobby
+    /// </summary>
+    /// <param name="displayName"> player's display name in the lobby </param>
+    [Command]
+    private void CmdSetDisplayName(string displayName)
+    {
+        DisplayName = displayName;
+    }
+
+    /// <summary>
+    /// Sends over the network a command to the server when a player has readied up 
+    /// </summary>
+    [Command]
+    public void CmdReadyUp()
+    {
+        IsReady = !IsReady;
+
+        Room.NotifyPlayersOfReadyState();
+    }
+    
+    [Command]
+    public void CmdStartGame()
+    {
+        if (Room.RoomPlayers[0].connectionToClient != connectionToClient)
+        {
+            return;
+        }
+        //Room.StartGame();
+    }
+}
