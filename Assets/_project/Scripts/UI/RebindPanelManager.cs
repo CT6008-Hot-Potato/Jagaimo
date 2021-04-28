f1a7a8acd8dfeb3e3a6d4c168815c54e5b371034
CABgames <S1707038@glos.ac.uk>
2021-04-28 18:42:37 +0100

    Rebinding

--- Assets/_project/Scripts/UI/RebindPanelManager.cs
+++ Assets/_project/Scripts/UI/RebindPanelManager.cs
@@ -10,18 +10,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
 
 public class RebindPanelManager : MonoBehaviour
 {
     [SerializeField] GameObject rebindObject;
     public InputActionAsset ControlScheme;  // put new Input manager here
-
+    private PlayerInput inputAction;
     public string MapName; // Name of the list of inputs being changed. (player 1, player 2, etc)
 
     
     // Start is called before the first frame update
     void Start()
     {
+        inputAction = transform.root.GetComponent<PlayerInput>();
         foreach (InputAction i in ControlScheme.FindActionMap(MapName).actions)
         {
             if (i.type == InputActionType.Button)
@@ -30,17 +33,12 @@ public class RebindPanelManager : MonoBehaviour
 
                 if (j.TryGetComponent(out RebindButton button))
                 {
-
+                    j.GetComponentInChildren<Button>().onClick.AddListener(button.ButtonPressedStartRebind);
+                    button.pI = inputAction;
                     button.UpdateDisplay(i.name, i.GetBindingDisplayString(InputBinding.DisplayStringOptions.DontIncludeInteractions), i);
-
                 }
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
