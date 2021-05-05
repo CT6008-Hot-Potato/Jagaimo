0fc84d266f350dabb9a53edf394d03ff535a97b6
Charles Carter <47723974+cmcrter@users.noreply.github.com>
2021-05-05 06:48:20 +0100

    Working on power ups spawning

--- Assets/_project/Scripts/Monobehaviour/PowerUpTrigger.cs
+++ Assets/_project/Scripts/Monobehaviour/PowerUpTrigger.cs
@@ -5,15 +5,23 @@
 // Brief: The base script for powerups to use
 //////////////////////////////////////////////////////////// 
 
+//The namespaces used
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUpTrigger : MonoBehaviour
 {
+    //Noting down where this power up spawned
+    [SerializeField]
+    private int SpawningSpotID;
+
     //Need a way of identifying what this powerup does
     [SerializeField]
     private int PowerUp_ID;
+
+    [SerializeField]
+    private ArenaManager spawningManager;
 
     //If something enters the trigger collider
     private void OnTriggerEnter(Collider other)
@@ -24,7 +32,20 @@ public class PowerUpTrigger : MonoBehaviour
             //Tell their manager that they've triggered this powerup
             //other.GetComponent<PlayerPowerUpManager>().TriggerPowerUp(PowerUp_ID);
 
+            //If there is an arena manager associated with this power up
+            if (spawningManager)
+            {
+                //Tell it that this spot has cleared up
+                spawningManager.PowerUpPickedUp(SpawningSpotID);
+            }
+
             gameObject.SetActive(false);
         }
+    }
+
+    //Passing a reference to the arena manager to the power up
+    public void SetArenaManager(ArenaManager aManager)
+    {
+        spawningManager = aManager;
     }
 }
