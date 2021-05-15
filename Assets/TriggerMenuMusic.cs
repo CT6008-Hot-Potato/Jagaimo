9a08aa9ff1fa520ed824d200015c1904d35be967
James Bradbury <unkoun98@gmail.com>
2021-05-15 12:28:09 +0100

    added main menu music

---
+++ Assets/TriggerMenuMusic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class TriggerMenuMusic : MonoBehaviour
+{
+    [SerializeField] AudioClip DefaultMusic;
+    [SerializeField] AudioMixerGroup MixerGroup;
+    private void OnEnable()
+    {
+        if (Camera.main.gameObject.TryGetComponent( out AudioSource audio))
+        {
+
+        }
+        else
+        {
+            AudioSource MenuMusic = Camera.main.gameObject.AddComponent<AudioSource>();
+            MenuMusic.clip = DefaultMusic;
+            MenuMusic.loop = true;
+            MenuMusic.outputAudioMixerGroup = MixerGroup;
+            MenuMusic.Play();
+        }
+    }
+}
