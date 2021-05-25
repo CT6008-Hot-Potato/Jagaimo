936c5dbbb0cc4e08de2a946f64b48f0f13e6e574
Charles Carter <47723974+cmcrter@users.noreply.github.com>
2021-05-25 08:14:07 +0100

    Basic Credits screen/script

---
+++ Assets/_project/Scripts/UI/CreditsScreen.cs
@@ -0,0 +1,60 @@
+﻿////////////////////////////////////////////////////////////
+// File: CreditsScreen.cs
+// Author: Charles Carter
+// Date Created: 25/05/21
+// Brief: The script for the credits screen in the main menu
+////////////////////////////////////////////////////////////
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsScreen : MonoBehaviour
+{
+    #region Variables Needed
+
+    [SerializeField]
+    private ScrollRect scrollingText;
+
+    bool isLocked = false;
+    [SerializeField]
+    private float fCreditsTime = 10f;
+    #endregion
+
+    private void Awake()
+    {
+        if (!scrollingText && Debug.isDebugBuild)
+        {
+            Debug.Log("No scroll rect selected for credits", this);
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (!scrollingText)
+        {
+            scrollingText = GetComponentInChildren<ScrollRect>();
+        }
+
+        StartCoroutine(Co_ScrollingText(fCreditsTime));
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(Co_ScrollingText(fCreditsTime));
+
+        if (scrollingText)
+        {
+            scrollingText.verticalNormalizedPosition = 1;
+        }
+    }
+
+    private IEnumerator Co_ScrollingText(float textTimer)
+    {
+        for (float t = 1; t < 0; t += Time.deltaTime / textTimer)
+        {
+            scrollingText.verticalNormalizedPosition = t;
+            yield return null;
+        }
+    }
+}
