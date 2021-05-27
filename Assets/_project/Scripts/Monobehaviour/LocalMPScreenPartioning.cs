110fabf740ded63561803db7a4353c1fc3fb90e1
CABgames <S1707038@glos.ac.uk>
2021-05-27 15:20:35 +0100

    Player ai progress

--- Assets/_project/Scripts/Monobehaviour/LocalMPScreenPartioning.cs
+++ Assets/_project/Scripts/Monobehaviour/LocalMPScreenPartioning.cs
@@ -10,10 +10,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class LocalMPScreenPartioning : MonoBehaviour
-{
+public class LocalMPScreenPartioning : MonoBehaviour {
     #region Variables Needed
-
+    //Variables:
     public int playerIndex;
     [SerializeField]
     private PlayerCamera[] playerCameras;
@@ -39,48 +38,40 @@ public class LocalMPScreenPartioning : MonoBehaviour
     #endregion
 
     #region Unity Methods
-
-    private void Awake()
-    {
+    //On awake get needed components
+    private void Awake() {
         settings = GameSettingsContainer.instance;
         manager = manager ?? GetComponent<PlayerInputManager>();
 
         //If there are settings to take values from
-        if (settings)
-        {
-            if (settings.iPlayercount > 1)
-            {
-                ExpectedPlayerCount = settings.iPlayercount;
+        if (settings) {
+            if (settings.iPlayercount > 1) {
 
+                ExpectedPlayerCount = settings.iPlayercount;
                 singleLocalPlayer = false;
 
-                for (int i = 0; i < settings.iPlayercount; ++i)
-                {
+                for (int i = 0; i < settings.iPlayercount; ++i) {
+
                     int tempIndex = settings.LocalPlayerInputs[i].playerIndex;
                     int tempSplitscreenIndex = settings.LocalPlayerInputs[i].splitScreenIndex;
                     string tempControlScheme = settings.LocalPlayerInputs[i].currentControlScheme;
                     latestDeviceJoined = settings.LocalPlayerInputs[i].devices[0];
-
                     Destroy(settings.LocalPlayerInputs[i].gameObject);
-
                     manager.JoinPlayer(tempIndex, tempSplitscreenIndex, tempControlScheme);
                 }
             }
-            else if (settings.iPlayercount == 1)
-            {
+            else if (settings.iPlayercount == 1) {
                 singleLocalPlayer = true;
-
                 int tempIndex = settings.LocalPlayerInputs[0].playerIndex;
                 int tempSplitscreenIndex = settings.LocalPlayerInputs[0].splitScreenIndex;
                 string tempControlScheme = settings.LocalPlayerInputs[0].currentControlScheme;
                 latestDeviceJoined = settings.LocalPlayerInputs[0].devices[0];
-
+                //Destroy local player index for this
                 Destroy(settings.LocalPlayerInputs[0].gameObject);
-
+                //Join the single player
                 manager.JoinPlayer(tempIndex, tempSplitscreenIndex, tempControlScheme);
             }
-            else
-            {
+            else {
                 //No players joined, so it was single player from the main menu
                 singleLocalPlayer = true;
                 singlePlayer = Instantiate(playerPrefab, new Vector3(0, 25, 0), playerPrefab.transform.rotation);
@@ -88,14 +79,12 @@ public class LocalMPScreenPartioning : MonoBehaviour
             }
         }
         //Was played from the scene, anyone can join
-        else if (singleLocalPlayer)
-        {
+        else if (singleLocalPlayer) {
             //playerManager.SetActive(false);
             singlePlayer = Instantiate(playerPrefab, new Vector3(0, 25, 0), playerPrefab.transform.rotation);
             singlePlayer.GetComponent<CharacterManager>().UnLockPlayer();
         }
-        else
-        {
+        else {
             manager.EnableJoining();
         }
     }
@@ -104,39 +93,35 @@ public class LocalMPScreenPartioning : MonoBehaviour
 
     #region Public Methods
 
-    public void OnPlayerJoined(PlayerInput playerInput)
-    {     
+    //When a player joined adjusr the player index correctly including checking to give the correct camera sensitivity which is needed for them
+    public void OnPlayerJoined(PlayerInput playerInput) {
+        
         //Getting the camera from the player that joined
         PlayerCamera camera = playerInput.GetComponent<PlayerCamera>();
 
         //Making sure it has a camera
-        if (camera)
-        {
+        if (camera) {
             //Using the relevant functions
             camera.playerIndex = playerInput.playerIndex;
             camera.SetPlayerMask();
 
             //Checking player device
-            if (latestDeviceJoined != null)
-            {
-                if (latestDeviceJoined.name != "Keyboard" && latestDeviceJoined.name != "Mouse")
-                {
+            if (latestDeviceJoined != null) {
+                if (latestDeviceJoined.name != "Keyboard" && latestDeviceJoined.name != "Mouse") {
                     //Gamepad used
                     camera.useControllerSensitivity = true;
                 }
-                else
-                {
+                else {
                     //Keyboard used
                     camera.useControllerSensitivity = false;
                 }
             }
-
+            //Add new player camera
             newPlayerCameras.Add(camera);
         }
 
         //This is the last player to join
-        if (newPlayerCameras.Count >= ExpectedPlayerCount && !singleLocalPlayer)
-        {
+        if (newPlayerCameras.Count >= ExpectedPlayerCount && !singleLocalPlayer) {
             SetScreenResolutions();
         }
     }
@@ -145,21 +130,22 @@ public class LocalMPScreenPartioning : MonoBehaviour
     #region Private Methods
 
     //Setting the resolutions of the cameras based on the amount of players
-    private void SetScreenResolutions()
-    {
-        switch (newPlayerCameras.Count)
-        {
+    private void SetScreenResolutions() {
+        //Switch statement checks the amount of new player cameras and adjust player camera rect accordingly
+        switch (newPlayerCameras.Count) {
+            //One player
             case 1:
                 newPlayerCameras[0].firstPersonCamera.rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
                 newPlayerCameras[0].thirdPersonCamera.rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
                 break;
+            //Two players
             case 2:
                 newPlayerCameras[0].firstPersonCamera.rect = new Rect(0.0f, 0.5f, 1.0f, 0.5f);
                 newPlayerCameras[0].thirdPersonCamera.rect = new Rect(0.0f, 0.5f, 1.0f, 0.5f);
-
                 newPlayerCameras[1].firstPersonCamera.rect = new Rect(0.0f, 0.0f, 1.0f, 0.5f);
                 newPlayerCameras[1].thirdPersonCamera.rect = new Rect(0.0f, 0.0f, 1.0f, 0.5f);
                 break;
+            //Three players
             case 3:
                 newPlayerCameras[0].firstPersonCamera.rect = new Rect(0.0f, 0.5f, 1.0f, 0.5f);
                 newPlayerCameras[0].thirdPersonCamera.rect = new Rect(0.0f, 0.5f, 1.0f, 0.5f);
@@ -168,6 +154,7 @@ public class LocalMPScreenPartioning : MonoBehaviour
                 newPlayerCameras[2].firstPersonCamera.rect = new Rect(0.5f, 0.0f, 0.5f, 0.5f);
                 newPlayerCameras[2].thirdPersonCamera.rect = new Rect(0.5f, 0.0f, 0.5f, 0.5f);
                 break;
+            //Four players
             case 4:
                 newPlayerCameras[0].firstPersonCamera.rect = new Rect(0.0f, 0.5f, 0.5f, 0.5f);
                 newPlayerCameras[0].thirdPersonCamera.rect = new Rect(0.0f, 0.5f, 0.5f, 0.5f);
@@ -177,10 +164,9 @@ public class LocalMPScreenPartioning : MonoBehaviour
                 newPlayerCameras[2].thirdPersonCamera.rect = new Rect(0.0f, 0.0f, 0.5f, 0.5f);
                 newPlayerCameras[3].firstPersonCamera.rect = new Rect(0.5f, 0.0f, 0.5f, 0.5f);
                 newPlayerCameras[3].thirdPersonCamera.rect = new Rect(0.5f, 0.0f, 0.5f, 0.5f);
-                break;
+                break; 
             default:
-                if (Debug.isDebugBuild)
-                {
+                if (Debug.isDebugBuild) {
                     Debug.Log("There are no cameras", this);
                 }
                 break;
