c47d8e220cfcab4731c00edca4ef31651ecb5d17
CABgames <S1707038@glos.ac.uk>
2021-06-27 00:45:05 +0100

    GOAP
    Implemented basic code from spy vs guards module goap

---
+++ Assets/_project/Scripts/GOAP/Data/Components/DocumentComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DocumentComponent : MonoBehaviour
+{
+    //Variables
+    public int numDocuments = 5;
+    private string startingText;
+    private TextMeshPro text;
+    private string currentText;
+
+    //Start function gets text component needed
+    private void Start()
+    {
+        text = GetComponentInChildren<TextMeshPro>();
+        startingText = text.text;
+    }
+
+    //Update function changes text when it needed to be updated
+    private void Update()
+    {
+        if (text.text != (startingText + "\n" + "Amount of documents: " + "\n" + numDocuments))
+        {
+            text.text = startingText + "\n" + "Amount of documents: " + "\n" + numDocuments;
+        }
+    }
+}
---
+++ Assets/_project/Scripts/GOAP/Data/Components/RadioComponent.cs
@@ -0,0 +1,33 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///Name: RadioComponent.cs 
+///Created by: Charlie Bullock based on GOAP example given in CT6024
+///Description: This component can be tampered with or detected when tampered with
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class RadioComponent : MonoBehaviour
+{
+    //Variables
+    public bool tamperedWith = false;
+    private string startingText;
+    private TextMeshPro text;
+    private string currentText;
+
+    //Start function gets component for text mesh pro
+    private void Start()
+    {
+        text = GetComponentInChildren<TextMeshPro>();
+        startingText = text.text;
+    }
+
+    //Update function will check if text needs updating
+    private void Update()
+    {
+        if (text.text != (startingText + "\n" + "Tampered with: " + "\n" + tamperedWith))
+        {
+            text.text = startingText + "\n" + "Tampered with: " + "\n" + tamperedWith;
+        }
+    }
+}
